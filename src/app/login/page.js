0c434ba8afc3d5f49a0c0360c9d662356ce50c0b
nubisub <hendraksm18@mail.ugm.ac.id>
2023-06-07 00:57:41 +0700

    add feature
    add sidebar layout, add main page search, notif and profile, add routing

---
+++ src/app/login/page.js
@@ -0,0 +1,18 @@
+"use client";
+
+import Image from "next/image";
+import { Popover } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Fragment } from "react";
+import { Transition } from "@headlessui/react";
+import { Menu } from "@headlessui/react";
+import { EditIcon } from "@heroicons/react/outline";
+import { DuplicateIcon } from "@heroicons/react/outline";
+
+export default function Home() {
+	return (
+		<main className="absolute min-w-[calc(100vw-250px)] min-h-screen left-[250px] ">
+			<h1 className="text-3xl font-bold">Login</h1>
+		</main>
+	);
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,278 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import { Popover } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Fragment } from "react";
+import { Transition } from "@headlessui/react";
+import { Menu } from "@headlessui/react";
+import { EditIcon } from "@heroicons/react/outline";
+import { DuplicateIcon } from "@heroicons/react/outline";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<main className="absolute min-w-[calc(100vw-250px)] min-h-screen left-[250px]">
+			<div className="flex justify-between items-center border-b border-gray-50 border-opacity-20 p-12 py-4">
+				{/* Searchbar */}
+				<div className="">
+					<div className="relative flex items-center w-full h-12 rounded-full focus-within:shadow-lg bg-white overflow-hidden">
+						<div className="grid place-items-center h-full w-12 ml-2 text-gray-400">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-6 w-6"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+								/>
+							</svg>
+						</div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+						<input
+							className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+							type="text"
+							id="search"
+							placeholder="Cari Sesuatu ..."
+						/>
+					</div>
+				</div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+				{/* Akun dan Notifikasi */}
+				<div className="flex gap-x-4">
+					<Menu as="div" className="relative inline-block text-left">
+						<div>
+							<Menu.Button className="inline-flex w-full justify-center rounded-full bg-black bg-opacity-20 p-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									strokeWidth={1.5}
+									stroke="currentColor"
+									className="w-5 h-5"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+									/>
+								</svg>
+							</Menu.Button>
+						</div>
+						<Transition
+							as={Fragment}
+							enter="transition ease-out duration-100"
+							enterFrom="transform opacity-0 scale-95"
+							enterTo="transform opacity-100 scale-100"
+							leave="transition ease-in duration-75"
+							leaveFrom="transform opacity-100 scale-100"
+							leaveTo="transform opacity-0 scale-95"
+						>
+							<Menu.Items className="absolute overflow-y-scroll max-h-[400px] right-0 mt-2 min-w-[450px] origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+								<div>
+									<div className="flex justify-between items-center p-4">
+										<span className="text-sm">
+											You have <b>13</b> notifications.
+										</span>
+									</div>
+									<Menu.Item className="p-4 border-b">
+										{({ active }) => (
+											<button
+												className={`${
+													active ? "bg-slate-100" : "text-gray-900"
+												} group flex w-full flex-col rounded-md px-4 py-2 text-sm`}
+											>
+												<h2 className="font-semibold">John Snow</h2>
+												<span className="text-gray-500 text-left">
+													Membuat artikel yang berjudul Hasil Uji Kompetensi
+													Jabatan Fungsional Statistisi dan Pranata Komputer
+													periode Juni 2022
+												</span>
+											</button>
+										)}
+									</Menu.Item>
+									<Menu.Item className="p-4 border-b">
+										{({ active }) => (
+											<button
+												className={`${
+													active ? "bg-slate-100" : "text-gray-900"
+												} group flex w-full flex-col rounded-md px-4 py-2 text-sm`}
+											>
+												<h2 className="font-semibold">John Snow</h2>
+												<span className="text-gray-500 text-left">
+													Membuat artikel yang berjudul Hasil Uji Kompetensi
+													Jabatan Fungsional Statistisi dan Pranata Komputer
+													periode Juni 2022
+												</span>
+											</button>
+										)}
+									</Menu.Item>
+									<Menu.Item className="p-4 border-b">
+										{({ active }) => (
+											<button
+												className={`${
+													active ? "bg-slate-100" : "text-gray-900"
+												} group flex w-full flex-col rounded-md px-4 py-2 text-sm`}
+											>
+												<h2 className="font-semibold">John Snow</h2>
+												<span className="text-gray-500 text-left">
+													Membuat artikel yang berjudul Hasil Uji Kompetensi
+													Jabatan Fungsional Statistisi dan Pranata Komputer
+													periode Juni 2022
+												</span>
+											</button>
+										)}
+									</Menu.Item>
+									<Menu.Item className="p-4 border-b">
+										{({ active }) => (
+											<button
+												className={`${
+													active ? "bg-slate-100" : "text-gray-900"
+												} group flex w-full flex-col rounded-md px-4 py-2 text-sm`}
+											>
+												<h2 className="font-semibold">John Snow</h2>
+												<span className="text-gray-500 text-left">
+													Membuat artikel yang berjudul Hasil Uji Kompetensi
+													Jabatan Fungsional Statistisi dan Pranata Komputer
+													periode Juni 2022
+												</span>
+											</button>
+										)}
+									</Menu.Item>
+								</div>
+								<div className="px-1 py-1"></div>
+							</Menu.Items>
+						</Transition>
+					</Menu>
+					<Menu as="div" className="relative inline-block text-left">
+						<div>
+							<Menu.Button className="inline-flex w-full justify-center rounded-full bg-black bg-opacity-20 p-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 24 24"
+									fill="currentColor"
+									class="w-5 h-5"
+								>
+									<path
+										fill-rule="evenodd"
+										d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+										clip-rule="evenodd"
+									/>
+								</svg>
+							</Menu.Button>
+						</div>
+						<Transition
+							as={Fragment}
+							enter="transition ease-out duration-100"
+							enterFrom="transform opacity-0 scale-95"
+							enterTo="transform opacity-100 scale-100"
+							leave="transition ease-in duration-75"
+							leaveFrom="transform opacity-100 scale-100"
+							leaveTo="transform opacity-0 scale-95"
+						>
+							<Menu.Items className="absolute right-0 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+								<div className="px-1 py-1 ">
+									<Menu.Item>
+										{({ active }) => (
+											<button
+												className={`${
+													active ? "bg-slate-100" : "text-gray-900"
+												} group flex w-full flex-col rounded-md px-4 py-2 text-sm`}
+											>
+												My Profile
+											</button>
+										)}
+									</Menu.Item>
+									<Menu.Item>
+										{({ active }) => (
+											<button
+												className={`${
+													active ? "bg-slate-100" : "text-gray-900"
+												} group flex w-full flex-col rounded-md px-4 py-2 text-sm`}
+											>
+												Setting
+											</button>
+										)}
+									</Menu.Item>
+									<Menu.Item>
+										{({ active }) => (
+											<button
+												className={`${
+													active ? "bg-slate-100" : "text-gray-900"
+												} group flex w-full flex-col rounded-md px-4 py-2 text-sm`}
+											>
+												Activity
+											</button>
+										)}
+									</Menu.Item>
+									<Menu.Item>
+										{({ active }) => (
+											<button
+												className={`${
+													active ? "bg-slate-100" : "text-gray-900"
+												} group flex w-full flex-col rounded-md px-4 py-2 text-sm`}
+											>
+												Logout
+											</button>
+										)}
+									</Menu.Item>
+								</div>
+							</Menu.Items>
+						</Transition>
+					</Menu>
+				</div>
+			</div>
+			<div className="p-10 w-full">
+				<p className="text-wrap">
+					The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum
+					dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+					commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+					velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+					occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				</p>
+				<p className="text-wrap">
+					The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum
+					dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+					commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+					velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+					occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				</p>
+				<p className="text-wrap">
+					The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum
+					dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+					commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+					velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+					occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				</p>
+				<p className="text-wrap">
+					The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum
+					dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+					commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+					velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+					occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				</p>
+				<p className="text-wrap">
+					The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum
+					dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+					commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+					velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+					occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				</p>
+			</div>
+		</main>
+	);
 }
